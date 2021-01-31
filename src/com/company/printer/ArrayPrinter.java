13dd66a1d4e0aca26799b62df1080422b9d57732
tatianaserdiuk <tetianaskh@gmsil.com>
2021-01-31 02:12:07 +0200

    Task 12

---
+++ src/com/company/printer/ArrayPrinter.java
@@ -0,0 +1,20 @@
+package com.company.printer;
+
+import java.util.Arrays;
+
+/**
+ * Описать класс ArrayPrinter, содержащий 2 метода
+ * 1) публичный метод print(int[] array) выводящий массив
+ * 2) защищенный метод printElem(int elem) для вывода одного элемента (в базовом классе пуст)
+ */
+public class ArrayPrinter {
+    public void print(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(Arrays.toString(array));
+        }
+    }
+
+    protected void printElem(int elem) {
+        System.out.println(elem);
+    }
+}
